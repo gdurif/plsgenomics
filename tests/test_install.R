d33d3ec377a763608e302b02f0de3e0d0711570c
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2015-02-19 12:58:29 +0100

    testing installation

---
+++ tests/test_install.R
@@ -0,0 +1,8 @@
+##### testing package installation
+
+
+system("R CMD INSTALL --library=/home/durif/source_code/tests plsgenomics_1.3.tar.gz")
+
+
+
+library(plsgenomics, lib.loc="/home/durif/source_code/tests")
