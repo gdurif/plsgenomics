9b000baa14452808c5feff1f583291fb615bc730
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2015-02-20 14:28:10 +0100

    annotate the code

--- computeMD5sum.R
+++ computeMD5sum.R
@@ -2,12 +2,23 @@
 
 library(tools)
 
+## list of files in package
 fileList = system(paste0("git ls-files pkg"), intern=TRUE)
 
+## md5sum creation
 md5sumList = md5sum(fileList)
 str(md5sumList)
 
+## output in data.frame
 md5sumTable = data.frame(MD5=md5sumList, files=names(md5sumList), stringsAsFactors=FALSE)
 rownames(md5sumTable)= NULL
 
-md5sumTable$Files = sapply(md5sumTable$Files, function(word) sub(pattern="pkg/", replacement="*", x=word, perl=TRUE, fixed=TRUE))
+## file name at the good format
+md5sumTable$files = sapply(md5sumTable$files, function(word) sub(pattern="pkg/", replacement="*", x=word))
+
+## remove the MD5sum for the MD5 file
+index.MD5 = which(md5sumTable$files == "*MD5")
+md5sumTable = md5sumTable[-index.MD5,]
+
+## save the table in the MD5 file
+write.table(x=md5sumTable, file="./pkg/MD5", quote=FALSE, sep=" ", row.names=FALSE, col.names=FALSE)
