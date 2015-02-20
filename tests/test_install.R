b83512f880260c0499b14cff4cdf166ce07dbc46
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2015-02-20 14:38:11 +0100

    all process withMD5 sum and tar.gz compression

--- tests/test_install.R
+++ tests/test_install.R
@@ -1,6 +1,13 @@
 ##### testing package installation
 
+## recreating the MD5 file
+source("computeMD5sum.R")
 
+## recreating the package source archive
+system("tar -zcvf plsgenomics_1.3.tar.gz pkg/")
+
+## installing this new version of the package
 system("R CMD INSTALL --library=/home/durif/source_code/tests plsgenomics_1.3.tar.gz")
 
+## loading this new version of the package
 library(plsgenomics, lib.loc="/home/durif/source_code/tests")
