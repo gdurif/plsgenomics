e0e17d44b2223397b012bc2ef656f2bfbb4f8623
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2015-02-20 14:43:18 +0100

    clear environement and remove previous installation of the package

--- tests/test_install.R
+++ tests/test_install.R
@@ -1,7 +1,13 @@
 ##### testing package installation
 
+## clear environment
+rm(list=ls())
+
 ## recreating the MD5 file
 source("computeMD5sum.R")
+
+## remove previous installation test if exists
+system("if [ -d /home/durif/source_code/tests/plsgenomics ]; then rm -r /home/durif/source_code/tests/plsgenomics; fi;")
 
 ## recreating the package source archive
 system("tar -zcvf plsgenomics_1.3.tar.gz pkg/")
