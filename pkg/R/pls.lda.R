f2974c2a6ac4e45a6a1b69177f26e69f05149275
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2015-10-21 10:48:06 +0200

    pls and lda object in output, choose whether CV in lda to compute posterior in input

--- pkg/R/pls.lda.R
+++ pkg/R/pls.lda.R
@@ -23,7 +23,7 @@
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ### MA 02111-1307, USA
 
-pls.lda<-function(Xtrain, Ytrain, Xtest=NULL, ncomp, nruncv=0, alpha=2/3, priors=NULL)
+pls.lda<-function(Xtrain, Ytrain, Xtest=NULL, ncomp, nruncv=0, alpha=2/3, priors=NULL, CVlda=TRUE)
 {
      ntrain<-nrow(Xtrain)
      Ytrain<-as.factor(Ytrain)
