43ff4fc32121300300a8f5270cd6f6a167f2acdc
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-01 16:32:17 +0200

    doc generator regarding internal functions

---
+++ pkg/R/internal.R
@@ -0,0 +1,74 @@
+### internal.R  (2017-08)
+###
+###    Internal plsgenomics functions
+###
+### Copyright 2017-08 Ghislain DURIF
+###
+###
+### This file is part of the `plsgenomics' library for R and related languages.
+### It is made available under the terms of the GNU General Public
+### License, version 2, or at your option, any later version,
+### incorporated herein by reference.
+### 
+### This program is distributed in the hope that it will be
+### useful, but WITHOUT ANY WARRANTY; without even the implied
+### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+### PURPOSE.  See the GNU General Public License for more
+### details.
+### 
+### You should have received a copy of the GNU General Public
+### License along with this program; if not, write to the Free
+### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+### MA 02111-1307, USA
+
+#' Internal Functions for the 'plsgenomics' package
+#' 
+#' These are not to be called by the user.
+#' @keywords internal
+#' @rdname plsgenomics-internal
+#' @name plsgenomics-internal
+#' @docType package
+#' 
+#' @export standard.simpls
+#' @export unitr.simpls
+#' @export transformy
+#' @export pls.regression.sample
+#' @export pls.lda.sample
+#' @export gsim.aux
+#' @export mgsimaux
+#' @export rplsaux
+#' @export mrplsaux
+#' @export hplugin
+#' @export wirrls
+#' @export mwirrls
+#' 
+#' @export ust
+#' @export ust.adapt
+#' @export wpls
+#' @export spls.aux
+#' @export spls.in
+#' @export logit.spls.aux
+#' @export multinom.spls.aux
+#' 
+#' @aliases 
+#' standard.simpls
+#' unitr.simpls
+#' transformy
+#' pls.regression.sample
+#' pls.lda.sample
+#' gsim.aux
+#' mgsimaux
+#' rplsaux
+#' mrplsaux
+#' hplugin
+#' wirrls
+#' mwirrls
+#' ust
+#' ust.adapt
+#' wpls
+#' spls.aux
+#' spls.in
+#' logit.spls.aux
+#' multinom.spls.aux
+#' 
+NULL
