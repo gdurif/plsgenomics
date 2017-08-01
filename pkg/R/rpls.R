34c6239d04d8ae50ec13a965f83c482a599ffe3d
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-01 11:55:56 +0200

    add the alias logit.pls that correspond to the naming in the adaptive spls article

--- pkg/R/rpls.R
+++ pkg/R/rpls.R
@@ -21,7 +21,7 @@
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ### MA 02111-1307, USA
 
-rpls <- function (Ytrain,Xtrain,Lambda,ncomp,Xtest=NULL,NbIterMax=50) {
+rpls <- logit.pls <- function (Ytrain,Xtrain,Lambda,ncomp,Xtest=NULL,NbIterMax=50) {
      
      ##    INPUT VARIABLES
      #########################
--- pkg/R/rpls.cv.R
+++ pkg/R/rpls.cv.R
@@ -22,7 +22,7 @@
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ### MA 02111-1307, USA
 
-rpls.cv <- function (Ytrain, Xtrain, LambdaRange, ncompMax, NbIterMax=50, ncores=1) {
+rpls.cv <- logit.pls.cv <- function (Ytrain, Xtrain, LambdaRange, ncompMax, NbIterMax=50, ncores=1) {
 
      ##    INPUT VARIABLES
      #########################
