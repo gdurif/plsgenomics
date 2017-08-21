4aceb01bbe8e8e0294197ef34d7b69a61aae193b
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-21 15:51:44 +0200

    stability selection procedure logit.spls.stab

--- pkg/R/logit.spls.stab.R
+++ pkg/R/logit.spls.stab.R
@@ -1,6 +1,6 @@
 ### logit.spls.stab.R  (2014-10)
 ###
-###    Tuning parameters (ncomp, lambda.l1, lambda.ridge) for Ridge Iteratively Reweighted Least Squares followed by Adaptive Sparse PLS regression for binary response, by K-fold cross-validation
+###    Stability selection procedure for logit.spls
 ###
 ### Copyright 2014-10 Ghislain DURIF
 ###
@@ -21,7 +21,153 @@
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ### MA 02111-1307, USA
 
-
+#' @title
+#' Stability selection procedure to select covariates for the LOGIT-SPLS method
+#' @aliases logit.spls.stab
+#' 
+#' @description 
+#' The function \code{logit.spls.stab} train a logit-spls model for each 
+#' candidate values \code{(ncomp, lambda.l1, lambda.ridge)} of hyper-parameters 
+#' on multiple sub-samplings in the data. The stability selection procedure 
+#' selects the covariates that are selected by most of the models among the 
+#' grid of hyper-parameters, following the procedure described in 
+#' Durif et al.  (2017). Candidates values for \code{ncomp}, \code{lambda.l1} 
+#' and \code{lambda.l2} are respectively given by 
+#' the input arguments \code{ncomp.range}, \code{lambda.l1.range} 
+#' and \code{lambda.l2.range}.
+#' 
+#' 
+#' @details
+#' The columns of the data matrices \code{X} may not be standardized, 
+#' since standardizing is performed by the function \code{logit.spls.stab} 
+#' as a preliminary step. 
+#' 
+#' The procedure is described in Durif et al. (2017). The stability selection 
+#' procedure can be summarize as follow (c.f. Meinshausen and Buhlmann, 2010).
+#' 
+#' (i) For each candidate values \code{(ncomp, lambda.l1, lambda.ridge)} of 
+#' hyper-parameters, a logit-SPLS is #' trained on \code{nresamp} resamplings 
+#' of the data. Then, for each triplet \code{(ncomp, lambda.l1, lambda.ridge)}, 
+#' the probability that a covariate (i.e. a column in \code{X}) is selected is 
+#' computed among the resamplings.
+#' 
+#' (ii) Eventually, the set of "stable selected" variables corresponds to the 
+#' set of covariates that were selected by most of the training among the 
+#' grid of hyper-parameters candidate values.
+#' 
+#' This function achieves the first step (i) of the stability selection 
+#' procedure. The second step (ii) is achieved by the function 
+#' \code{\link{stability.selection}}
+#' 
+#' This procedures uses the \code{mclapply} from the \code{parallel} package, 
+#' available on GNU/Linux and MacOS. Users of Microsoft Windows can refer to 
+#' the README file in the source to be able to use a mclapply type function.
+#' 
+#' @param X a (n x p) data matrix of predictors. \code{X} must be a matrix. 
+#' Each row corresponds to an observation and each column to a 
+#' predictor variable.
+#' @param Y a (n) vector of (continuous) responses. \code{Y} must be a 
+#' vector or a one column matrix. It contains the response variable for 
+#' each observation.
+#' @param lambda.ridge.range a vector of positive real values. 
+#' \code{lambda.ridge} is the Ridge regularization parameter for the 
+#' RIRLS algorithm (see details), the optimal value will be chosen among
+#' \code{lambda.ridge.range}.
+#' @param lambda.l1.range a vecor of positive real values, in [0,1]. 
+#' \code{lambda.l1} is the sparse penalty parameter for the dimension 
+#' reduction step by sparse PLS (see details), the optimal value will be 
+#' chosen among \code{lambda.l1.range}.
+#' @param ncomp.range a vector of positive integers. \code{ncomp} is the 
+#' number of PLS components. The optimal value will be chosen 
+#' among \code{ncomp.range}.
+#' @param adapt a boolean value, indicating whether the sparse PLS selection 
+#' step sould be adaptive or not (see details).
+#' @param maxIter a positive integer, the maximal number of iterations in the 
+#' RIRLS algorithm (see details).
+#' @param svd.decompose a boolean parameter. \code{svd.decompose} indicates 
+#' wether or not the predictor matrix \code{Xtrain} should be decomposed by 
+#' SVD (singular values decomposition) for the RIRLS step (see details).
+#' @param ncores a positve integer, indicating the number of cores that the 
+#' cross-validation is allowed to use for parallel computation (see details).
+#' @param nresamp number of resamplings of the data to estimate the probility 
+#' of selection for each covariate, default is 100.
+#' @param center.X a boolean value indicating whether the data matrices 
+#' \code{Xtrain} and \code{Xtest} (if provided) should be centered or not.
+#' @param scale.X a boolean value indicating whether the data matrices 
+#' \code{Xtrain} and \code{Xtest} (if provided) should be scaled or not 
+#' (\code{scale.X=TRUE} implies \code{center.X=TRUE}) in the spls step.
+#' @param weighted.center a boolean value indicating whether the centering 
+#' should take into account the weighted l2 metric or not in the SPLS step.
+#' @param seed a positive integer value (default is NULL). If non NULL, 
+#' the seed for pseudo-random number generation is set accordingly.
+#' @param verbose a boolean parameter indicating the verbosity.
+#' 
+#' @return An object with the following attributes
+#' \item{q.Lambda}{A table with values of q.Lambda (c.f. Durif 
+#' et al. (2017) for the notation), being the averaged number of covariates
+#' selected among the entire grid of hyper-parameters candidates values,
+#' for increasing size of hyper-parameter grid.}
+#' \item{probs.lambda}{A table with estimated probability of selection for each 
+#' covariates depending on the candidates values for hyper-parameters.}
+#' 
+#' @references 
+#' Durif G., Modolo L., Michaelsson J., Mold J. E., Lambert-Lacroix S., 
+#' Picard F. (2017). High Dimensional Classification with combined Adaptive 
+#' Sparse PLS and Logistic Regression, (in prep), 
+#' available on (\url{http://arxiv.org/abs/1502.05933}).
+#' 
+#' Meinshausen, N., Buhlmann P. (2010). Stability Selection. Journal of the 
+#' Royal Statistical Society: Series B (Statistical Methodology) 
+#' 72, no. 4, 417–473.
+#' 
+#' @author
+#' Ghislain Durif (\url{http://thoth.inrialpes.fr/people/gdurif/}).
+#' 
+#' @seealso \code{\link{logit.spls}}, \code{\link{stability.selection}}, 
+#' \code{\link{stability.selection.heatmap}}
+#' 
+#' @examples
+#' ### load plsgenomics library
+#' library(plsgenomics)
+#' 
+#' ### generating data
+#' n <- 100
+#' p <- 100
+#' sample1 <- sample.bin(n=n, p=p, kstar=10, lstar=2, 
+#'                       beta.min=0.25, beta.max=0.75, mean.H=0.2, 
+#'                       sigma.H=10, sigma.F=5)
+#' 
+#' X <- sample1$X
+#' Y <- sample1$Y
+#' 
+#' ### pertinent covariates id
+#' sample1$sel
+#' 
+#' ### hyper-parameters values to test
+#' lambda.l1.range <- seq(0.05,0.95,by=0.1) # between 0 and 1
+#' ncomp.range <- 1:10
+#' # log-linear range between 0.01 a,d 1000 for lambda.ridge.range
+#' logspace <- function( d1, d2, n) exp(log(10)*seq(d1, d2, length.out=n))
+#' lambda.ridge.range <- signif(logspace(d1 <- -2, d2 <- 3, n=21), digits=3)
+#' 
+#' ### tuning the hyper-parameters
+#' stab1 <- logit.spls.stab(X=X, Y=Y, lambda.ridge.range=lambda.ridge.range, 
+#'                          lambda.l1.range=lambda.l1.range, 
+#'                          ncomp.range=ncomp.range, 
+#'                          adapt=TRUE, maxIter=100, svd.decompose=TRUE, 
+#'                          ncores=1, nresamp=100)
+#'                        
+#' str(stab1)
+#' 
+#' ### heatmap of estimated probabilities
+#' \dontrun{
+#' stab.sel.heatmap(stab1)
+#' }
+#' 
+#' ### selected covariates
+#' stability.selection(stab1, piThreshold=0.6, rhoError=10)
+#' 
+#' @export
 logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range, 
                             ncomp.range, 
                             adapt=TRUE, maxIter=100, svd.decompose=TRUE, 
@@ -63,12 +209,12 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
      # if multicategorical response
      if(length(table(Y)) > 2) {
           warning("message from logit.spls.stab: multicategorical response")
-          results = m.logit.spls.stab(X=X, Y=Y, lambda.ridge.range=lambda.ridge.range, lambda.l1.range=lambda.l1.range, 
-                                      ncomp.range=ncomp.range, 
-                                      adapt=adapt, maxIter=maxIter, svd.decompose=svd.decompose, 
-                                      ncores=ncores, nresamp=nresamp, nfolds=nfolds, 
-                                      center.X=center.X, scale.X=scale.X, weighted.center=weighted.center, 
-                                      seed=seed, verbose=verbose)
+          results <- multinom.spls.stab(X=X, Y=Y, lambda.ridge.range=lambda.ridge.range, lambda.l1.range=lambda.l1.range,
+                                        ncomp.range=ncomp.range,
+                                        adapt=adapt, maxIter=maxIter, svd.decompose=svd.decompose,
+                                        ncores=ncores, nresamp=nresamp,
+                                        center.X=center.X, scale.X=scale.X, weighted.center=weighted.center,
+                                        seed=seed, verbose=verbose)
           return(results)
      }
      
@@ -128,9 +274,9 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
           stop("message from logit.spls.stab: ncores is not of valid type")
      }
      
-     # necessary to insure that both classes are represented in each folds
-     if( any(as.vector(table(Y))<nfolds)) {
-          stop("message from logit.spls.stab: there is a class defined by Y that has less members than the number of folds nfold")
+     # nresamp
+     if ((!is.numeric(nresamp)) || (round(nresamp)-nresamp!=0) || (nresamp<1)) {
+          stop("message from logit.spls.stab: nresamp is not of valid type")
      }
      
      
@@ -171,7 +317,20 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
           }
           
           if(test==100) {
-               stop("message from logit.spls.stab: error in subsampling, a sub-sample has only one Y-label.")
+               ## at least one observation of each class in the train set (if random splitting did not work)
+               ind0 = which(Y==0)
+               ind1 = which(Y==1)
+               index.train = sample(ind0, size=1)
+               index.train = c(index.train, sample(ind1, size=1))
+               index.train = c(index.train, sample((1:n)[which(!(1:n) %in% index.train)], size=ntrain-2))
+               
+               index.test = (1:n)[-index.train]
+               
+               Xtrain = X[index.train,]
+               Ytrain = Y[index.train]
+               
+               Xtest = X[index.test,]
+               Ytest = Y[index.test]
           }
           
           #### hyper-parameter grid
@@ -180,11 +339,15 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
                                    ncomp=ncomp.range, KEEP.OUT.ATTRS=FALSE)
           
           #### fit the model for the different lambda.l1
-          grid_out <- as.matrix( Reduce("rbind", lapply(split(paramGrid, f=row.names(paramGrid)), function(gridRow) {
+          grid_out <- as.matrix( Reduce("rbind", lapply(1:nrow(paramGrid), function(gridRow) {
                
-               lambdaL1 <- gridRow$lambdaL1
-               lambdaL2 <- gridRow$lambdaL2
-               ncomp <- gridRow$ncomp
+               # lambdaL1 <- gridRow$lambdaL1
+               # lambdaL2 <- gridRow$lambdaL2
+               # ncomp <- gridRow$ncomp
+               
+               lambdaL1 <- paramGrid$lambdaL1[gridRow]
+               lambdaL2 <- paramGrid$lambdaL2[gridRow]
+               ncomp <- paramGrid$ncomp[gridRow]
                
                ## fit the model for the chosen lambda ridge
                fit_out <- logit.spls(Xtrain=Xtrain, Ytrain=Ytrain, 
@@ -242,6 +405,7 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
           return(tmp4)
           
      }, mc.cores=ncores, mc.silent=!verbose)))
+     
      tmp_qLambda <- data.frame(tmp_qLambda)
      colnames(tmp_qLambda) <- c("lambdaL1", "lambdaL2", "ncomp", "id.samp", "qLambda")
      
@@ -262,7 +426,7 @@ logit.spls.stab <- function(X, Y, lambda.ridge.range, lambda.l1.range,
      
      
      ##### return
-     return(list(qLambda=qLambda, probs.lambda=probs_lambda))
+     return(list(q.Lambda=qLambda, probs.lambda=probs_lambda))
      
 }
 
