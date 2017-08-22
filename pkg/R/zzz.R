074d0f0b0b12e20f569353cbcd81656961eb5b39
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-22 17:36:50 +0200

    use automatic startup message function

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-     print("'plsganomics' package")
-     print("C++ based sparse PLS routines will soon be available on the CRAN in the new 'fastPLS' package.")
+     packageStartupMessage("C++ based sparse PLS routines will soon be available on the CRAN in the new 'fastPLS' package.")
      
      ## setting BLAS and OPENMP number of threads (modified in each function)
      blas_set_num_threads(1)
