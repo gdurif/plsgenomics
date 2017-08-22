5b15ed2e1051fde3a5c313ff5b6fd37dbb096e1d
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-22 15:22:15 +0200

    scripts to build packages

--- admin/computeMD5sum.R
+++ admin/computeMD5sum.R
@@ -2,8 +2,15 @@
 
 library(tools)
 
+## PATH
+args = commandArgs(trailingOnly=TRUE);
+path = args
+
+## dir
+pkgDir = paste0(path, "/pkg")
+
 ## list of files in package
-fileList = system(paste0("git ls-files pkg"), intern=TRUE)
+fileList = system(paste0("git ls-files ", pkgDir), intern=TRUE)
 
 ## md5sum creation
 md5sumList = md5sum(fileList)
@@ -21,4 +28,4 @@ index.MD5 = which(md5sumTable$files == "*MD5")
 md5sumTable = md5sumTable[-index.MD5,]
 
 ## save the table in the MD5 file
-write.table(x=md5sumTable, file="./pkg/MD5", quote=FALSE, sep=" ", row.names=FALSE, col.names=FALSE)
+write.table(x=md5sumTable, file=paste0(pkgDir, "/MD5"), quote=FALSE, sep=" ", row.names=FALSE, col.names=FALSE)
