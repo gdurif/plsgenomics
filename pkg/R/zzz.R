118b4d9b8672489a6180e3ba9eb9da8bd265a9fb
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2017-08-29 14:07:07 +0200

    startup message in '.onAttach' instead of '.onLoad'

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,6 +1,8 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
      packageStartupMessage("C++ based sparse PLS routines will soon be available on the CRAN in the new 'fastPLS' package.")
-     
+}
+
+.onLoad <- function(libname, pkgname) {
      ## setting BLAS and OPENMP number of threads (modified in each function)
      blas_set_num_threads(1)
      omp_set_num_threads(1)
