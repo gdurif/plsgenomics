60c9a997530118162da03accd1a65b71bb8f9c40
GD <gd.dev@libertymail.net>
2020-07-31 12:21:36 +0200

    minor update

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-     packageStartupMessage("For any news related to the 'plsgenomics' package (update, corrected bugs), please check https://gitlab.inria.fr/gdurif/plsgenomics")
+     packageStartupMessage("For any news related to the 'plsgenomics' package (update, corrected bugs), please check https://github.com/gdurif/plsgenomics")
      packageStartupMessage("C++ based sparse PLS routines will soon be available on the CRAN in the new 'fastPLS' package.")
 }
 
